04b53b6aaa2d5e63edf5781d36d7494c9d69e585
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-14 15:12:03 +0000

    Sun, Jul 14, 2024, 6:12 PM +03:00

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 int main(void)
 {
     string answer;
-    get_string("What's your name");
-    printf("Hello,%s/n",answer);
+    answer = get_string("What's your name ");
+    printf("Hello, %s\n", answer);
 }
