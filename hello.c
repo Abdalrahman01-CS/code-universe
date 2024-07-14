703d59a936f36ea02e2378cce54551d9cd310ce8
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-14 15:45:24 +0000

    Sun, Jul 14, 2024, 6:45 PM +03:00

--- hello.c
+++ hello.c
@@ -2,7 +2,20 @@
 #include <cs50.h>
 int main(void)
 {
-    string answer;
-    answer = get_string("What's your name ");
-    printf("Hello, %s\n", answer);
+     int x,y;
+     x = get_int("What's x? ");
+     y = get_int("What's y? ");
+
+     if (x > y)
+     {
+        printf("x is greater than y\n");
+     }
+    else if (x < y)
+     {
+        printf("x is less than y\n");
+     }
+     else
+     {
+        printf("x is equal y\n");
+     }
 }
