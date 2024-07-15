c6bf9b4005642d02f1584199badebc8e0bde8486
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-15 22:11:25 +0000

    Tue, Jul 16, 2024, 1:11 AM +03:00

--- credit.c
+++ credit.c
@@ -1,8 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+int digit(long credit_card);
 int main(void)
 {
     long credit = get_long("Credit Card: ");
-    
+    int digit_sum = 
+
+}
+int digit(long credit_card)
+{
+    int sum = 0;
+    while(credit_card > 0)
+    {
+        int l_digit = credit_card % 10;
+        credit_card/=10;
+    }
+    return sum;
 }
 
