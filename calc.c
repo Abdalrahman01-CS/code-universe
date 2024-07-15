c191bf652760208efc35fee3709a28512de83ca9
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-15 13:19:57 +0000

    Mon, Jul 15, 2024, 4:19 PM +03:00

--- calc.c
+++ calc.c
@@ -1,15 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-int add(void);
+
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
+    float x = get_float("x: ");
+    float y = get_float("y: ");
 
+    printf("%f\n",x/y);
 
 }
 
-int add(void)
-{
-    printf("%i\n",x+y);
-}
+
