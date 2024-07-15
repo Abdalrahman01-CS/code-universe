136aba168743f6c65ea935395464780d8352b999
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-15 21:39:17 +0000

    Tue, Jul 16, 2024, 12:39 AM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,7 +11,7 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        for(int k = 0; k < n; k++)
+        for(int k = 0; k < n - i - 1; k++)
         {
             printf(" ");
         }
