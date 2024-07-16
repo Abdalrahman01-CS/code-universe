20777e2880c2877e4b3f9649c9ceaddd18e3800b
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-16 12:43:56 +0000

    Tue, Jul 16, 2024, 3:43 PM +03:00

--- m.c
+++ m.c
@@ -7,6 +7,7 @@ int main (void)
      int n = get_int("Height: ");
      for(int i = 0; i < n; i++)
      {
+
         h(i + 1);
      }
 
@@ -17,6 +18,11 @@ void h(int space , int num)
     for(int i = 0; i < num; i++)
     {
             printf("#");
+
+            for(int space = 0; space < i; space++)
+            {
+                printf(" ");
+            }
     }
 
         printf("\n");
