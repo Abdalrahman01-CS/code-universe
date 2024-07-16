62ddd8c6f25530a9e83e2cb5ab26b3c4e4837a9b
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-16 12:22:50 +0000

    Tue, Jul 16, 2024, 3:22 PM +03:00

---
+++ m.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int number(int num);
+int main (void)
+{
+     int n = get_int("Height: ");
+}    number(int n);
+
+int number(int num)
+{
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j < num; j++)
+        {
+            printf("#\n");
+        }
+
+        printf("\n");
+    }
+}
+
