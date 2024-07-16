00f9998cc71ba3571016541202207daf44a34181
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-16 12:45:46 +0000

    Tue, Jul 16, 2024, 3:45 PM +03:00

--- m.c
+++ m.c
@@ -1,28 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void h(int space , int num);
+void h(int num);
 int main (void)
 {
      int n = get_int("Height: ");
      for(int i = 0; i < n; i++)
      {
-
         h(i + 1);
      }
 
 }
 
-void h(int space , int num)
+void h(int num)
 {
     for(int i = 0; i < num; i++)
     {
             printf("#");
-
-            for(int space = 0; space < i; space++)
-            {
-                printf(" ");
-            }
     }
 
         printf("\n");
