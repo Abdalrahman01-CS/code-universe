65f7d5fd548b168ca9adbedfda9ed47496aafef7
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-18 13:07:11 +0000

    Thu, Jul 18, 2024, 4:07 PM +03:00

--- length.c
+++ length.c
@@ -1,14 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int s_length(string s);
+
 int main(void)
 {
     string name = get_string("Name: ");
+    int length = s_length(name);
+    printf("%i\n", length);
 
+}
+
+int s_length(string s)
+{
     int n = 0;
     while (name[n] != '\0')
     {
         n++;
     }
-    printf("%i\n", n);
+    return n;
 }
