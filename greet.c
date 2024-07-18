6270bd29042b19f740e2c80bfc4dcd73bf069ed0
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-18 16:38:47 +0000

    Thu, Jul 18, 2024, 7:38 PM +03:00

--- greet.c
+++ greet.c
@@ -1,11 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
-int main(int argc, string argv[][])
+int main(int argc, string argv[])
 {
     for (int i = 0; i < argc; i++)
     {
 
-    printf("hello, %s\n", argv[1]);
-    
+    printf("hello, %s\n", argv[i]);
+
     }
 }
