a50cde8e92e627aed63f05964199a15cce9323be
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-19 16:11:51 +0000

    Fri, Jul 19, 2024, 7:11 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -45,11 +45,7 @@ int compute_score(string word)
         {
             score += POINTS[word[i] - 'A'];
         }
-        else if (islower(word[i]))
-        {
-            score += POINTS[word[i] - 'a'];
-        }
-    }
+     }
 
     return score;
 }
