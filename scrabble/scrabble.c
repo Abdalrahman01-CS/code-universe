2b671a9c2030d5f93c44297b61fc5b015abbf8c3
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-19 16:13:37 +0000

    Fri, Jul 19, 2024, 7:13 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -37,7 +37,7 @@ int compute_score(string word)
 {
     // Keep track of score
     int score = 0;
-word
+    toupper(word);
     // Compute score for each character
     for (int i = 0, len = strlen(word); i < len; i++)
     {
