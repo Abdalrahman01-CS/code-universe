738bb21260c989f6abe7233057730972bfce3aee
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-19 16:34:42 +0000

    Fri, Jul 19, 2024, 7:34 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,10 +2,9 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 // Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+static int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
 
@@ -38,13 +37,18 @@ int compute_score(string word)
 {
     // Keep track of score
     int score = 0;
-toupper(word);
+
     // Compute score for each character
     for (int i = 0, len = strlen(word); i < len; i++)
     {
-
+        if (isupper(word[i]))
+        {
             score += POINTS[word[i] - 'A'];
-
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
     }
 
     return score;
