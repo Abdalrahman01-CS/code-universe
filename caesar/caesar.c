4f7996a555302fd5588ce873141b7d1116b2f9f0
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-21 11:07:54 +0000

    Sun, Jul 21, 2024, 2:07 PM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,1 +1,22 @@
-#include
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar KEY");
+        return 1;
+    }
+
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        if (!(isdigit(argv[1][i])))
+        {
+            printf("Usage: ./caesar KEY");
+            return 1;
+        }
+    }
+}
