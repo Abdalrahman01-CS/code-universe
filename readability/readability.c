85b67e677583776a1d5c87bca83d05c7abb629db
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-21 10:27:19 +0000

    Sun, Jul 21, 2024, 1:27 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -57,7 +57,9 @@ int count_sentences(string text)
     int s = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (text[i] == "\0")
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
         s++;
+        }
     }
 }
