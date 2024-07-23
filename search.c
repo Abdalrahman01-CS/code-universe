03dc2643e53f0c7236ab3558c10d8b52c243db7b
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-23 21:04:28 +0000

    Wed, Jul 24, 2024, 12:04 AM +03:00

--- search.c
+++ search.c
@@ -1,9 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
-    string 
+    string s = get_string("String: ");
+    for (int i = 0; i < 6; i++)
+    {
+        if(strings[i] == s)
+        {
+            printf("Found: \n");
+            return 0;
+        }
+    }
+    printf("Not found: \n");
+            return 1;
 }
