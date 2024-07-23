9c1a5f70e6780701373c4483ab7211dde2003d02
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-23 21:36:44 +0000

    Wed, Jul 24, 2024, 12:36 AM +03:00

--- phonebook.c
+++ phonebook.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 
+type struct
+{
+    string name;
+    string number;
+}
+person;
+
 int main(void)
 {
-     string names[] = {"Carter", "David", "John"};
-     string numbers[] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
+      person people[3];
 
      string name = get_string("Name: ");
      for (int i = 0; i < 3; i++)
