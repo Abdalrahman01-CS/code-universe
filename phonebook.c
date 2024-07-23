accbb9eb612e9dd09dd7d37c2dac5a620208cf81
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-23 21:41:10 +0000

    Wed, Jul 24, 2024, 12:41 AM +03:00

--- phonebook.c
+++ phonebook.c
@@ -16,12 +16,11 @@ int main(void)
      string name = get_string("Name: ");
      for (int i = 0; i < 3; i++)
      {
-          if (strcmp(names[i], name) == 0)
+          if (strcmp(people[i].name, name) == 0)
           {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
           }
-
      }
      printf("Not found\n");
             return 1;
