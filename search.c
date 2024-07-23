e90e58f7fe0185f9836f11d1f15540e6128d7059
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-23 20:59:11 +0000

    Tue, Jul 23, 2024, 11:59 PM +03:00

--- search.c
+++ search.c
@@ -3,18 +3,7 @@
 
 int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
-    int n = get_int("Number: ");
-    for (int i = 0; i < 7; i++)
-    {
-        if(numbers[i] == n)
-        {
-            printf("Found\n");
-            return 0;
-        }
-
-    }
-    printf("Not found\n");
-    return 1;
+    string 
 }
