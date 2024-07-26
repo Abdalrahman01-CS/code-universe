882474157e334675875e383496203cd989eb81e2
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-26 22:26:06 +0000

    Sat, Jul 27, 2024, 1:26 AM +03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,12 +68,11 @@ bool vote(string name)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes++;
-            return true;
         }
-
+        return true;
     }
     return false;
 }
