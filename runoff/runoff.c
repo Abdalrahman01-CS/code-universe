babed2f413dc2659f2b915cc09a113d0aad0632c
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-26 22:57:45 +0000

    Sat, Jul 27, 2024, 1:57 AM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,9 +143,9 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
-    for (int i = 0; i < voter_counter; i++)
+    for (int i = 0; i < voter_count; i++)
     {
-        for (j = 0; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (candidates[preferences[i][j]].eliminated == false)
             {
