21cc3a5ef0c73867d1bbebcf798c1a086e565ad8
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-28 21:08:08 +0000

    Mon, Jul 29, 2024, 12:08 AM +03:00

--- garbage.c
+++ garbage.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    int scores[1024];
+    string scores[1024];
     for (int i = 0; i < 1024; i++)
     {
-        printf("%i\n", scores[i]);
+        printf("%s\n", scores);
     }
 }
