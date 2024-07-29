c350e7e2de75731c42ed47fe59d1c99f509521d4
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-29 15:01:06 +0000

    Mon, Jul 29, 2024, 6:01 PM +03:00

--- cp.c
+++ cp.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
+
+typedef uint8_t by;
+
+int main(int argc, char *argv[])
+{
+    FILE *src = fopen(argv[1], "rb");
+    FILE *dst = fopen(argv[2], "wb");
+
+    by b;
+
+    while (fread(&b, ))
+}
