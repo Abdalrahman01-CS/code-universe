64606056d66beb01180f93f891a034c7c9acd77c
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-29 15:32:47 +0000

    Mon, Jul 29, 2024, 6:32 PM +03:00

--- cp.c
+++ cp.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef uint8_t by;
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
     FILE *src = fopen(argv[1], "rb");
     FILE *dst = fopen(argv[2], "wb");
 
-    by b;
+    BYTE b;
 
     while (fread(&b, sizeof(b), 1, src) != 0)
     {
