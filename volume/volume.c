65235360ed90ddb1bd3f673ed0bf191a285d2e0f
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-07-30 19:30:02 +0000

    Tue, Jul 30, 2024, 10:30 PM +03:00

--- volume/volume.c
+++ volume/volume.c
@@ -1,21 +1,43 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <stdio.h>
+// Modifies the volume of an audio file
 
-typedef uint8_t BYTE;
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
 
 int main(int argc, char *argv[])
 {
-    FILE *file = fopen("phonebook.csv", "a");
-    if (file == NULL)
+    // Check command-line arguments
+    if (argc != 4)
     {
+        printf("Usage: ./volume input.wav output.wav factor\n");
         return 1;
     }
 
-    char *name = get_string("Name: ");
-    char *number = get_string("Number: ");
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
 
-    fprintf(file, "%s, %s\n", name, number);
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
 
-    fclose(file);
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+
+    // TODO: Read samples from input file and write updated data to output file
+
+    // Close files
+    fclose(input);
+    fclose(output);
 }
