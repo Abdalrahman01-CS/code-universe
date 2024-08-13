23f788b5fe252d293c42ece5e75163465f8e1fa9
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-13 15:20:15 +0000

    Tue, Aug 13, 2024, 6:20 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -100,7 +100,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width / 2; j++)
+        for (int j = 0; j < width; j++)
         {
             image[i][j] = image_replica[i][j];
         }
