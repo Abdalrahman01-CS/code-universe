603925871b3ad1a9bdb9a7c40f87c36b08b944ba
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-13 14:19:41 +0000

    Tue, Aug 13, 2024, 5:19 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -57,7 +57,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0;j<width/2; j++)
         {
-
+temp=image[i][j];
+image[i][width-j-1]
         }
     }
     return;
