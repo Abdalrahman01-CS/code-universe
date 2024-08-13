8add625636f229e21803ce82b0ace25a6a9fe1b7
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-13 15:04:18 +0000

    Tue, Aug 13, 2024, 6:04 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,8 +9,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int average_rgb =
-                round(image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
-            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = avg_rgb;
+                round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average_rgb;
         }
     }
     return;
