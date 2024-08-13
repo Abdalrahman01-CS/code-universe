aa8d06c3469f3810d9d84df2dfae6c77265bb96b
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-13 14:07:42 +0000

    Tue, Aug 13, 2024, 5:07 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -52,6 +52,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
 
