5ff7799148abce5cc879e988db74e36aa9ba632d
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-14 08:25:20 +0000

    Wed, Aug 14, 2024, 11:25 AM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,14 +10,14 @@ int main(int argc, char *argv[])
     }
     FILE *raw_f = fopen(argv[1], "r");
     uint_8 buffer[512];
-    bool  found_jpeg;
+    bool  found_jpeg; //false
     while(fread(buffer,1,512,raw_f))
     {
         if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             found_jpeg = true;
         }
-        else
+        else //delete
         {
             found_jpeg = false;
         }
