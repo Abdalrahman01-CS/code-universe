7aa6fc9f4af2c38363bdcfda75a5fbad76ac1324
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-18 14:41:08 +0000

    Sun, Aug 18, 2024, 5:41 PM +03:00

--- list.c
+++ list.c
@@ -1,39 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(void)
+typedef struct node
 {
-    int *list = malloc(3 * sizeof(int));
-    if(list == NULL)
+    int number;
+    struct node *next;
+} node;
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++)
     {
-        return 1;
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            // free
+        }
+
     }
-
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    int *tmp = malloc(4 * sizeof(int));
-    if (tmp == NULL)
-    {
-        free(list);
-        return 1;
-    }
-
-    for (int i = 0; i < 3; i++)
-    {
-        tmp[i] = list[i];
-    }
-    tmp[3] = 4;
-
-    free(list);
-    list = tmp;
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-
-    free(list);
-    return 0;
 }
