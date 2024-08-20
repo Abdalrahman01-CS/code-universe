7948bab4b2b7b46c9c1529af8eb4e59e61eb98ac
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-20 14:12:30 +0000

    Tue, Aug 20, 2024, 5:12 PM +03:00

--- list.c
+++ list.c
@@ -6,19 +6,9 @@ typedef struct node
     int number;
     struct node *next;
 } node;
+
 int main(int argc, char *argv[])
 {
-    node *list = NULL;
-
-    for (int i = 1; i < argc; i++)
-    {
-        int number = atoi(argv[i]);
-
-        node *n = malloc(sizeof(node));
-        if (n == NULL)
-        {
-            // free
-        }
-
-    }
+    
 }
+
