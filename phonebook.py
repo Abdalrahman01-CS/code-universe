02cee00ba14faa81c68342a8fb570dbe9c387559
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-30 21:03:09 +0000

    Sat, Aug 31, 2024, 12:03 AM +03:00

--- phonebook.py
+++ phonebook.py
@@ -1,13 +1,1 @@
-people = {
-      "Abdo": "+1-617-495-1000",
-      "Carter": "+1-617-495-1000",
-      "David": "+1-949-468-2750",
-}
-
-name = input("Name: ")
-
-if name in people:
-    print(f"Found {people[name]}")
-else:
-    print("Not found")
-
+ 
