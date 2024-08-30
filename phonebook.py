bc3bfed15c37e9749d2595d93a4f1a5702654c81
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-30 09:05:15 +0000

    Fri, Aug 30, 2024, 12:05 PM +03:00

--- phonebook.py
+++ phonebook.py
@@ -2,8 +2,10 @@ names = ["Carter", "David", "John"]
 
 name = input("Name: ")
 
-if name in names:
-    print("Found")
+for n in names:
+    if name == n:
+        print("Found")
+        break
 else:
     print("Not found")
 
