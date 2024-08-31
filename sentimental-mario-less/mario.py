ead27e32d6b419d71e5fdc36c28fcd0e77871651
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-31 18:21:16 +0000

    Sat, Aug 31, 2024, 9:21 PM +03:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,7 +2,7 @@ from cs50 import get_int
 
 while True:
     height = get_int("Height: ")
-    if height > 0:
+    if height > 0 and height < 9:
         break
 for level in range(height):
     print((height - level - 1) * " ", end="")
