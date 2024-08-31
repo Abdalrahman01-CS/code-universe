7365b0ac2f5367fff66adba34bffa0f2e8ec87fa
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-08-31 18:38:24 +0000

    Sat, Aug 31, 2024, 9:38 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,2 +1,24 @@
 from cs50 import get_float
 
+while True:
+    change = get_float("Change: ")
+    if change > 0:
+        break
+
+total_cents = int(change * 100)
+
+quarters = total_cents / 25
+total_cents -= quarters * 25
+
+dimes = total_cents / 10
+total_cents -= dimes * 10
+
+nickles = total_cents / 5
+total_cents -= nickles * 5
+
+pennies = total_cents
+
+total_cents = quarters + dimes + nickles + pennies
+
+print(total_cents)
+
