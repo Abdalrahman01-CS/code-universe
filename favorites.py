9f6bca1d5cc00daf1d495ceb984d1ced6b5e3251
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-04 14:30:18 +0000

    Wed, Sep 4, 2024, 5:30 PM +03:00

--- favorites.py
+++ favorites.py
@@ -2,5 +2,18 @@ import csv
 
 with open("favorites.csv") as file:
     reader = csv.DictReader(file)
+    scratch, c, python = 0, 0, 0
+
     for row in reader:
-         print(row["language"])
+        favorite = row["language"]
+        if favorite == "Scratch":
+            scratch += 1
+        elif favorite == "C":
+            c += 1
+        elif favorite == "Python":
+            python += 1
+
+print(f"Scratch: {scratch}")
+print(f"C: {c}")
+print(f"Python: {python}")
+
