d7de41b68988853c72b3fa117b69d4590b5bf707
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-06 16:29:25 +0000

    Fri, Sep 6, 2024, 7:29 PM +03:00

--- favorites.py
+++ favorites.py
@@ -2,7 +2,7 @@ from cs50 import SQL
 
 db = SQL("sqlite:///favorites.db")
 
-favorite = input("Favorit: ")
+favorite = input("Favorite: ")
 
 rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favorite)
 row = rows[0]
