6e0915f386d5e2133093ded848695ff91323636b
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-07 22:00:43 +0000

    Sun, Sep 8, 2024, 1:00 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -77,4 +77,13 @@ AND month = 7
 AND day = 28
 AND duration < 60)
 AND name IN
+(SELECT people.name FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_location = 'Leggett Street'
+AND atm_transactions.transaction_type = 'withdraw')
+AND name IN
 ()
