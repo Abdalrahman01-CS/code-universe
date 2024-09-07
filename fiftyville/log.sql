1bc43a38edb755e6b7f1f42ebcb01b1e3da82886
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-07 22:08:01 +0000

    Sun, Sep 8, 2024, 1:08 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,7 +3,7 @@
 SELECT description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28;
 
-SELECT transcript FROM
+SELECT transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 
 SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name
