97681295da03820728980b524915819cb308da0e
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-07 21:39:59 +0000

    Sun, Sep 8, 2024, 12:39 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,9 +6,29 @@ WHERE year = 2021 AND month = 7 AND day = 28;
 SELECT FROM
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 
-SELECT bakery_security_logs.activity
+SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name
+FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE bakery_security_logs.year = 2021
+AND bakery_security_logs.month = 7
+AND bakery_security_logs.day = 28
+AND bakery_security_logs.hour = 10
+AND bakery_security_logs.minute >= 15
+AND bakery_security_logs.minute <= 25;
 
-FROM
-JOIN ON
-WHERE
-AND AND AND AND AND
+SELECT people.name, atm_transactions.transaction_type FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_location = 'Leggett Street'
+AND atm_transactions.transaction_type = 'withdraw';
+
+SELECT caller, caller_name, receiver, receiver_name FROM phone_calls
+WHERE year = 2021
+AND month = 7
+AND day = 28
+AND duration < 60;
+
+
