e6b884a7395ef273f339baaec89fbe7f3c5c273f
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-07 20:45:28 +0000

    Sat, Sep 7, 2024, 11:45 PM +03:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,5 +1,6 @@
 SELECT title
 FROM movies
+JOIN ratings ON ratings.movie_id = movies.id
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON stars.person_id = people.id
 WHERE people.name = 'Chadwick Boseman'
