4b5ffc4ccff88af60fed24233e5e3fdef3b80daa
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-07 21:02:44 +0000

    Sun, Sep 8, 2024, 12:02 AM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,3 +5,9 @@ WHERE year = 2021 AND month = 7 AND day = 28;
 
 SELECT FROM
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
+
+SELECT bakery_security_logs.activity
+FROM
+JOIN ON
+WHERE
+AND AND AND AND AND
