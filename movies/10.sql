242b57f258a3c9b4d8aefcaf1238d2bac3b032cd
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-07 20:24:27 +0000

    Sat, Sep 7, 2024, 11:24 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -2,4 +2,5 @@ SELECT DISTINCT name
 FROM people
 JOIN directors ON directors.person_id = people.id
 JOIN movie ON movies.id = directors.movie_id
+JOIN ratings ON ratings.movie_id = movies.id
 WHERE rating >= 9.0;
