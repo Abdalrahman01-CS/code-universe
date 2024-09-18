d3ada240ad22a6d8433e570f83217a70615cddbb
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-18 14:26:22 +0000

    Wed, Sep 18, 2024, 5:26 PM +03:00

--- froshims/app.py
+++ froshims/app.py
@@ -9,8 +9,6 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name"):
+    if not request.form.get("name") or not request.form.get("sport"):
         return "failure"
-    elif not request.form.get("sport"):
-        return "failure"
-    return "success"
+     return "success"
