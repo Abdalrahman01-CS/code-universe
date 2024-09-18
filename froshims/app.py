487fba149c400820d31c0931f0acdaaf4c7dee24
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-18 15:16:33 +0000

    Wed, Sep 18, 2024, 6:16 PM +03:00

--- froshims/app.py
+++ froshims/app.py
@@ -6,7 +6,7 @@ SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports)
 
 
 @app.route("/register", methods=["POST"])
