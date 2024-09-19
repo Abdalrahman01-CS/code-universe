dfeec0d20dad95e1279cd29975e8310a1b1fcb0b
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-19 19:13:34 +0000

    Thu, Sep 19, 2024, 10:13 PM +03:00

--- login/app.py
+++ login/app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
+# Configure app
 app = Flask(__name__)
 
-SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
+# Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html", sports=SPORTS)
+    return render_template("index.html", name=session.get("name"))
 
 
-@app.route("/register", methods=["POST"])
-def register():
-    if not request.form.get("name"):
-         return render_template("failure.html")
-    for sport in request.form.getlist("sport"):
-        if sport not in SPORTS:
-            return render_template("failure.html")
-    return render_template("success.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
