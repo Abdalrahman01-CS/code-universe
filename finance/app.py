1dc30697f322808c8be7ed74169cb135cdc49b9c
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-21 14:55:57 +0000

    Sat, Sep 21, 2024, 5:55 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -63,7 +63,17 @@ def buy():
         transaction_value = shares * stock["price"]
 
         user_id = session["user_id"]
-        user_cash_db = db.execute("SELECT cash FROM users WHERE id=?", id=user_id)
+        user_cash_db = db.execute("SELECT cash FROM users WHERE id=:id", id=user_id)
+        user_cash = user_cash_db[0]["cash"]
+
+        if user_cash < transaction_value:
+            return apology("Not Enough Money")
+
+        uptd_cash = user_cash - transaction_value
+
+        db.execute("UPDATE users SET cash=? WHERE id=?", user_id)
+
+        date = datetime.datetime.now()
         return jsonify(user_cash_db)
 
 
