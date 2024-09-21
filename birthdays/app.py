7600d36738834da1c20972ae1d0d78389cf50a1a
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-21 11:32:18 +0000

    Sat, Sep 21, 2024, 2:32 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -25,36 +25,13 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form.get("name")
-    if not name:
-        return redirect("/")
 
-    month = request.form.get("month")
-    if not month:
         return redirect("/")
-    try:
-        month = int(month)
-    except ValueError:
-        return redirect("/")
-    if month < 1 or month > 12:
-        return redirect("/")
-
-    day = request.form.get("day")
-    if not day:
-        return redirect("/")
-    try:
-        day = int(day)
-    except ValueError:
-        return redirect("/")
-    if day < 1 or day > 31:
-        return redirect("/")
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
-    return redirect("/")
 
     else:
-        birthdays = db.execute("SELECT * FROM birthdays")
+
+        # TODO: Display the entries in the database on index.html
+
+        return render_template("index.html")
 
 
-return render_template("index.html", birthdays=birthdays)
-
-return render_template("index.html")
