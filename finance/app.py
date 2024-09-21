1021f97cad1ce1322ba4d848f7c5e62f450de491
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-21 14:42:52 +0000

    Sat, Sep 21, 2024, 5:42 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -63,7 +63,8 @@ def buy():
         transaction_value = shares * stock["price"]
 
         user_id = session["user_id"]
-        user_cash_db = db.execute("SELECT cash FROM users WHERE id")
+        user_cash_db = db.execute("SELECT cash FROM users WHERE id = ?", id=user_id)
+        return jsonify(user_cash_db)
 
 
 
