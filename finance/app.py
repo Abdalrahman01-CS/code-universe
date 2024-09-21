7a8333612644e750c10c22cb2edb94394e8e01bb
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-21 14:41:07 +0000

    Sat, Sep 21, 2024, 5:41 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -60,7 +60,10 @@ def buy():
         if shares < 0:
             return apology("Share Not Allowed")
 
+        transaction_value = shares * stock["price"]
 
+        user_id = session["user_id"]
+        user_cash_db = db.execute("SELECT cash FROM users WHERE id")
 
 
 
