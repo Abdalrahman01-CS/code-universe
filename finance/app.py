e592126a5affb5d7f5b2d334d55ebe6f2edb451b
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-21 15:03:32 +0000

    Sat, Sep 21, 2024, 6:03 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -75,10 +75,11 @@ def buy():
 
         date = datetime.datetime.now()
 
-        db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", username, hash)
-        return jsonify(user_cash_db)
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", user_id, stock["symbol"], shares, stock["price"], date)
 
+        flash("Bought!")
 
+        return redirect("/")
 
 @app.route("/history")
 @login_required
