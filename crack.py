5b182f2c028d7a9c5fffc3eaea93f8d4c8e615cb
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2024-09-24 13:35:10 +0000

    Tue, Sep 24, 2024, 4:35 PM +03:00

--- crack.py
+++ crack.py
@@ -1,6 +1,6 @@
-from string import ascii_letters
+from string import ascii_letters, digits, punctuation
 from itertools import product
 
-for passcode in product(ascii_letters, repeat=4):
+for passcode in product(ascii_letters + digits + punctuation, repeat=8):
     print(passcode)
 
